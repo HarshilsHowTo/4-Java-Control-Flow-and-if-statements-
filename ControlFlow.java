bdde8f2566eb18f2c8d16d469417f47532294d48
Harshil <harshils.how.to@gmail.com>
2018-07-07 20:24:01 +0100

    Java Control Flow code

---
+++ ControlFlow.java
@@ -0,0 +1,23 @@
+
+public class ControlFlow {
+
+    public static void main(String[] args) {
+
+        boolean lovesApples = false;
+        int num = 3;
+
+        System.out.println("This is the first statement");
+        System.out.println("This is the second statement");
+        if(num == 1){
+            System.out.println("I love apples");
+        }else{
+            System.out.println("I don't love apples ");
+        }
+        System.out.println("This is the last statement");
+
+
+    }
+
+
+
+}
